0b82ba45ef54a4910b670b7e7a684f716e910c0e
cra0zy <cra0zy@gmail.com>
2017-01-31 17:01:17 +0100

    Genesis 1:3

---
+++ GenexEditor.Windows/Program.cs
@@ -0,0 +1,16 @@
+﻿using Eto.Forms;
+using Eto;
+
+namespace GenexEditor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Application(Platform.Detect);
+            var win = new MainWindow();
+
+            app.Run(win);
+        }
+    }
+}
---
+++ GenexEditor/MainWindow.cs
@@ -0,0 +1,11 @@
+﻿using Eto.Forms;
+
+namespace GenexEditor
+{
+    public class MainWindow : Form
+    {
+        public MainWindow()
+        {
+        }
+    }
+}
